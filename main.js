f551e60be34e89f05e9874a75f8912012db6277a
taeeun98 <xodms6360@naver.com>
2025-06-08 01:55:01 +0900

    삭제 기능 추가가

---
+++ main.js
@@ -0,0 +1,31 @@
+function handleClick() {
+    //삭제 이벤트핸들러
+    const eliminationBtn = document.querySelectorAll('.elimination');
+    eliminationBtn.forEach(e => {
+        e.addEventListener('click', removeList);
+    });
+
+    // 전체삭제 이벤트핸들러
+    const allBtn = document.querySelector('.all');
+    allBtn.addEventListener('click', removeAll)
+}
+
+
+// 개별 삭제 함수
+function removeList(e) {
+    //console.log(e); //PointerEvent 객체
+    //console.log(e.target); //PointerEvent 객체가 가르키는 요소
+    e.target.closest('li').remove();
+}
+
+// 전체 삭제 함수
+function removeAll() {
+    const eliminationBtn = document.querySelectorAll('.elimination');
+
+    eliminationBtn.forEach(e => {
+        e.closest('li').remove();
+    });
+}
+
+
+handleClick();
