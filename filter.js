50c5a3f0a4a381bac5f514efebb79a39c72e930d
mtm1018 <mtm1018@naver.com>
2025-06-08 14:05:19 +0900

    필터 기능 추가

---
+++ filter.js
@@ -0,0 +1,37 @@
+// 필터링 버튼 누르면 active클래스 활성화 되는 기능
+function activeBtn() {
+  const filterBtn = document.querySelectorAll(".filter_box li > button");
+  const itemList = document.querySelectorAll(".list");
+  for (let btn of filterBtn) {
+    btn.addEventListener("click", function () {
+      for (let btn of filterBtn) {
+        if (btn !== this) {
+          btn.classList.remove("active");
+        } else if (btn === this) {
+          btn.classList.add("active");
+
+          // 버튼이 눌렸을때 필터링 되는 기능
+          for (const item of itemList) {
+            if (filterBtn[0] === this) {
+              item.style.display = "flex";
+            } else if (filterBtn[1] == this) {
+              if (!item.querySelector(".finish_txt")) {
+                item.style.display = "none";
+              } else {
+                item.style.display = "flex";
+              }
+            } else if (filterBtn[2] === this) {
+              if (item.querySelector(".finish_txt")) {
+                item.style.display = "none";
+              } else {
+                item.style.display = "flex";
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+}
+
+activeBtn();
