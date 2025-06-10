5a21f1012c70b9672cc35486ccf717c4559401d4
taeeun98 <xodms6360@naver.com>
2025-06-10 16:40:17 +0900

    삭제시 confirm 출력력

--- remove.js
+++ remove.js
@@ -13,17 +13,27 @@ function handleClick() {
 
 // 개별 삭제 함수
 function removeList(e) {
+    const isRemove = confirm('해당 항목을 삭제하시겠습니까?');
+    
+    if(!isRemove) return;
+
     const li = e.target.closest('li');
     toDoListArray = toDoListArray.filter((item) => item.id !== parseInt(li.dataset.id));
     //console.log(e); //PointerEvent 객체
     // console.log(e.target); //PointerEvent 객체가 가르키는 요소
     li.remove();
 
+
     updateTodoCount();
 }
 
 // 전체 삭제 함수
 function removeAll() {
+    const isRemove = confirm('전체 항목을 삭제하시겠습니까?');
+    
+    if(!isRemove) return;
+
+
     const eliminationBtn = document.querySelectorAll('.elimination');
 
     eliminationBtn.forEach(e => {
