5fb942bf7178ef7db462e98af2216e62d4f42598
mtm1018 <mtm1018@naver.com>
2025-06-10 17:43:57 +0900

    원래대로 돌려놨어요...

--- filter.js
+++ filter.js
@@ -1,6 +1,6 @@
 // 필터링 버튼 누르면 active클래스 활성화 되는 기능
-const filterBtn = document.querySelectorAll(".filter_box li > button");
 function activeBtn() {
+  const filterBtn = document.querySelectorAll(".filter_box li > button");
   for (let btn of filterBtn) {
     btn.addEventListener("click", function () {
       for (let btn of filterBtn) {
@@ -8,33 +8,30 @@ function activeBtn() {
           btn.classList.remove("active");
         } else if (btn === this) {
           btn.classList.add("active");
-          filteredItem();
+          const itemList = document.querySelectorAll(".list");
+
+          // 버튼이 눌렸을때 필터링 되는 기능
+          for (const item of itemList) {
+            if (filterBtn[0] === this) {
+              item.style.display = "flex";
+            } else if (filterBtn[1] == this) {
+              if (!item.querySelector(".finish_txt")) {
+                item.style.display = "none";
+              } else {
+                item.style.display = "flex";
+              }
+            } else if (filterBtn[2] === this) {
+              if (item.querySelector(".finish_txt")) {
+                item.style.display = "none";
+              } else {
+                item.style.display = "flex";
+              }
+            }
+          }
           updateTodoCount();
         }
       }
     });
-  }
-}
-
-// 버튼이 눌렸을때 필터링 되는 기능
-function filteredItem() {
-  const itemList = document.querySelectorAll(".list");
-  for (const item of itemList) {
-    if (filterBtn[0] === this) {
-      item.style.display = "flex";
-    } else if (filterBtn[1] == this) {
-      if (!item.querySelector(".finish_txt")) {
-        item.style.display = "none";
-      } else {
-        item.style.display = "flex";
-      }
-    } else if (filterBtn[2] === this) {
-      if (item.querySelector(".finish_txt")) {
-        item.style.display = "none";
-      } else {
-        item.style.display = "flex";
-      }
-    }
   }
 }
 
